369cff97519df13321da8c88e029fbdd27d5e93e
dcp2 <dcp2@ecomp.poli.br>
2018-11-20 11:40:02 -0300

    Inserindo uma função na classe''Util'' para normailzar os dados.

--- fatc_projects/clustering/java/src/br/clustering/data/LoadData.java
+++ fatc_projects/clustering/java/src/br/clustering/data/LoadData.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -15,7 +16,7 @@ import br.clustering.util.Util;
 
 public final class LoadData {
 	private static final LoadData INSTANCE = new LoadData();
-	private static BufferedReader bfr;
+	private static BufferedReader bfr = null;
 
 	// Private method.
 	private LoadData() {
@@ -28,17 +29,16 @@ public final class LoadData {
 
 	/**
 	 * Receives a path to the segmentation csv-like file.
-	 * 
+	 *
 	 * @param path
-	 * @return A map <y, X> with the targets (Integers) and the features (Double).
+	 * @return A map <id, Map<y, X>> with the the id and other map with targets (Integers) and the features (Double).
 	 * @throws IOException
 	 */
-	@SuppressWarnings("unchecked")
 	public Map<Integer, Map<Integer, List<Double>>> read(String path) throws IOException {
 		try {
 			bfr = new BufferedReader(new FileReader(new File(path)));
 
-			Map<Integer, Map<Integer, List<Double>>> myMap = new HashMap<>(); 
+			Map<Integer, Map<Integer, List<Double>>> myMap = new HashMap<>();
 			Iterator<String> it = bfr.lines().iterator();
 			it.next(); // Skiping first line
 			int cont = 0;
@@ -46,14 +46,14 @@ public final class LoadData {
 				String[] row = it.next().split(",");
 				List<Double> features = new ArrayList<Double>();
 				for (int i = 1; i < row.length; i++) {
-					features.add(Double.valueOf(row[i]));					
+					features.add(Double.valueOf(row[i]));
 				}
 				Map<Integer, List<Double>> map = new HashMap<>();
 				map.put(Util.targets.get(row[0].toLowerCase()), features);
 				myMap.put(cont, map);
 				cont++;
 			}
-			return myMap;			
+			return myMap;
 		} finally {
 			bfr.close();
 			bfr = null;
@@ -63,14 +63,24 @@ public final class LoadData {
 
 	public static void main(String[] args) throws IOException {
 		Map<Integer, Map<Integer, List<Double>>> map = LoadData.getInstance().read("../../../data/segmentation_2.test");
+
+		List<List<Double>> values = new ArrayList<>();
 		
 		for (Map<Integer, List<Double>> yX: map.values()) {
 			for (List<Double> example: yX.values()) {
-				System.out.println(example.stream().
-						map(Object::toString).
-						collect(Collectors.joining(" - ")));
+//				System.out.println(example.stream().
+//						map(Object::toString).
+//						collect(Collectors.joining(" - ")));
+				values.add(example);
 			}
 		}
-		System.out.println(map.keySet().size());
+//		System.out.println(map.keySet().size());
+		Double[][] matValues = Util.normalize(values);
+		for (Double[] features : matValues) {
+			System.out.println(Arrays.stream(features).
+					map(Object::toString).
+					collect(Collectors.joining(" - ")));
+		}
+
 	}
 }
