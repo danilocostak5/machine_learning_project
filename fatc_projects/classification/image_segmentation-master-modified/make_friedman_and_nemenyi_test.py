95ca8b07885bed814e52102b2d3674584dd78ff2
Dayvid Welles <wellescastro@gmail.com>
2018-11-25 01:06:27 -0300

    adjustment of the classifiers to compare now using chi-square-based critical value

--- fatc_projects/classification/image_segmentation-master-modified/make_friedman_and_nemenyi_test.py
+++ fatc_projects/classification/image_segmentation-master-modified/make_friedman_and_nemenyi_test.py
@@ -18,23 +18,30 @@ def find_index(array_var, title):
 for a in all_accuracies:
     size_k_fold = len(a['accuracies_bayes_rgb'])
     for i in xrange(size_k_fold):
+        bayes_complete = a['accuracies_bayes_complete'][i]
         bayes_rgb = a['accuracies_bayes_rgb'][i]
         bayes_shape = a['accuracies_bayes_shape'][i]
+        knn_complete = a['accuracies_knn_complete'][i]
         knn_rgb = a['accuracies_knn_rgb'][i]
         knn_shape = a['accuracies_knn_shape'][i]
-        majority = a['accuracies_majority'][i]
-        results_coupled = [['b_rgb', bayes_rgb], ['b_shape', bayes_shape],
-                           ['knn_rgb', knn_rgb], ['knn_shape', knn_shape],
-                           ['majority', majority]]
+        max_rule = a['accuracies_max_rule'][i]
+        results_coupled = [
+ 
+            ['b_complete', bayes_complete], ['b_rgb', bayes_rgb], ['b_shape', bayes_shape],
+            ['knn_complete', knn_complete], ['knn_rgb', knn_rgb], ['knn_shape', knn_shape],
+            ['max_rule', max_rule]
+            
+            ]
         rs = sorted(results_coupled, key= lambda x: x[1], reverse=True)
-        new_line = [find_index(rs, 'b_rgb'), find_index(rs, 'b_shape'),
-                    find_index(rs, 'knn_rgb'), find_index(rs, 'knn_shape'),
-                    find_index(rs, 'majority')]
+        new_line = [
+            find_index(rs, 'b_complete'), find_index(rs, 'b_rgb'), find_index(rs, 'b_shape'),
+            find_index(rs, 'knn_complete'), find_index(rs, 'knn_rgb'), find_index(rs, 'knn_shape'),
+            find_index(rs, 'max_rule')
+        ]
         ranks_matrix.append(new_line)
 
 ranks_np = np.array(ranks_matrix)
 mean_ranks = np.mean(ranks_np, axis=0)
-
 k = float(num_classifiers)
 
 sum_squared_ranks = float(np.sum(pow(mean_ranks, 2)))
@@ -46,7 +53,8 @@ print 'k', k
 print 'chi_square', chi_square
 print 'Statistic', statistic
 
-compare_val = 2.7858
+
+compare_val = 12.592  # https://www.medcalc.org/manual/chi-square-table.php (para alpha=0.05 e 7 classificadores - 1 graus de liberdade) temos
 
 if statistic > compare_val:
     print 'The classifiers are different'
@@ -54,11 +62,11 @@ else:
     print 'The classifiers are equal'
 
 #Nemenyi test
-q0 = 2.728
+q0 = 2.948 # seven models to compare
 critical_val = q0*np.sqrt(k*(k+1)/(6*float(tam_test)))
 print 'Critical values for Nemenyi test', critical_val
 
-name_of_classifiers = ['bayes rgb', 'bayes shape', 'knn rgb', 'knn shape', 'majority']
+name_of_classifiers = ['bayes_complete', 'bayes rgb', 'bayes shape', 'knn_complete', 'knn rgb', 'knn shape', 'max_rule']
 for i, n1 in enumerate(name_of_classifiers):
     for j , n2 in enumerate(name_of_classifiers):
         if i != j:
